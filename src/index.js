5fcd629b613ef809b3b445ae3e556a1f48122e25
Mark Ghian Francis <zetsuya6167@gmail.com>
2025-03-23 17:25:18 +0800

    Finish setting up the tab logic

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import "./style/style.css";
-import "./style/modernize.css"
+import "./style/modernize.css";
+import "./style/header.css";
+import HomePage from "./components/homepage";
 
+let mainContentContainer = document.querySelector("#contents");
+let navLinks = document.querySelectorAll(".nav__links li");
+
+navLinks.forEach((item) => {
+  item.addEventListener("click", function () {
+    mainContentContainer.innerHTML = "";
+
+    if (item.textContent === "Home") {
+      mainContentContainer.appendChild(HomePage());
+    } else if (item.textContent === "Menu") {
+      alert("Removed everything");
+    } else {
+      mainContentContainer.appendChild();
+    }
+  });
+});
